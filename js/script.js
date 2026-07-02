9bc95cf45aa7244c1ab4745479dbc6de7bfdc862
Suraj Kumar Shah <surajks.shah@gmail.com>
2026-07-02 16:55:40 +0545

    third

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log('Blog loaded');
+window.addEventListener("scroll",()=>{
+
+const nav=document.querySelector("header");
+
+if(window.scrollY>50){
+nav.classList.add("sticky");
+}else{
+nav.classList.remove("sticky");
+}
+
+});
